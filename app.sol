d72dd858561c3cb067811c51bdaa42273efb69cc
BugHunter <76444458+BugHunter021@users.noreply.github.com>
2025-11-09 21:46:59 +0330

    Update app.sol

--- app.sol
+++ app.sol
@@ -1,1 +1,35 @@
-app
+pragma solidity ^0.8.19;
+
+contract Challenge {
+    string private secret = "THM()";
+    bool private unlock_flag = false;
+    uint256 private code;
+    string private hint_text;
+    
+    constructor(string memory flag, string memory challenge_hint, uint256 challenge_code) {
+        secret = flag;
+        code = challenge_code;
+        hint_text = challenge_hint; // Passcode is in HINT Text
+    }
+    
+    function hint() external view returns (string memory) {
+        return hint_text;
+    }
+    
+    function unlock(uint256 input) external returns (bool) {
+        if (input == code) {
+            unlock_flag = true;
+            return true;
+        }
+        return false;
+    }
+    
+    function i$Solved() external view returns (bool) {
+        return unlock_flag;
+    }
+    
+    function getFlag() external view returns (string memory) {
+        require(unlock_flag, "Challenge not solved yet");
+        return secret;
+    }
+}
